6b52a57fd2cb085a80d67d20e3e87a37f7250808
Mishra <2488303@cognizant.com>
2026-08-29 14:40:01 +0530

    update HomePage for step Definitions

---
+++ src/test/java/StepDefinitions/HomePage.java
@@ -0,0 +1,15 @@
+package StepDefinitions;
+
+import Base.BaseTest;
+import Pages.*;
+import io.cucumber.java.en.*;
+
+import java.util.List;
+
+public class HomePage extends BaseTest {
+	
+	Products products;
+	
+	//@Given()
+
+}
